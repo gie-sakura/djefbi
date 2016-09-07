b04058f0fb3ab22759fd642702629ea1b6c5c6dc
gie_sakura <giegie_123@hotmail.com>
2016-09-07 18:11:52 +0000

    Reemplazo de imagenes y contenido

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ db.on('error', function () {
   throw new Error('unable to connect to database at ' + config.db);
 });
 
-
-
-
-
-
-
 var models = glob.sync(config.root + '/app/models/*.js');
 models.forEach(function (model) {
   require(model);
